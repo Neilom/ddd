e5232275edf49713d4bcbfa8914e711a0bb1d0ee
Neilom <leonardox.schmitt@gmail.com>
2023-11-12 17:56:02 -0300

    feat: create repository product

---
+++ src/domain/product/repository/product.repository.interface.ts
@@ -0,0 +1,4 @@
+import RepositoryInterface from '../../@shared/repository/repository.interface'
+import Product from "../../product/entity/product";
+
+export default interface ProductRepositoryInterface extends RepositoryInterface<Product> { }
