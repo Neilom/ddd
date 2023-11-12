1d9d28ae0faaad124974817e876268c19b557abc
Neilom <leonardox.schmitt@gmail.com>
2023-11-12 17:55:52 -0300

    feat: create repository customer

---
+++ src/domain/customer/entity/repository/customer.repository.interface.ts
@@ -0,0 +1,4 @@
+import RepositoryInterface from '../../../@shared/repository/repository.interface';
+import Customer from '../customer';
+
+export default interface CustomerRepositoryInterface extends RepositoryInterface<Customer> { }
