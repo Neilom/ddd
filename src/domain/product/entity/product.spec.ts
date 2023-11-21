e5c1a75ff61e45a96ab210b1afc9e3a74acd0733
Neilom <leonardox.schmitt@gmail.com>
2023-11-20 21:35:47 -0300

    feat: create factory product

--- src/domain/product/entity/product.spec.ts
+++ src/domain/product/entity/product.spec.ts
@@ -1,43 +1,33 @@
-import Product from "./product"
+import Product from "./product";
 
-describe('Product unit test', () => {
+describe("Product unit tests", () => {
+  it("should throw error when id is empty", () => {
+    expect(() => {
+      const product = new Product("", "Product 1", 100);
+    }).toThrowError("Id is required");
+  });
 
-    it('Should throw error when id is empty', () => {
-        expect(() => {
-            new Product('', 'produto1', 12);
-        }).toThrowError('Id is required')
-    });
+  it("should throw error when name is empty", () => {
+    expect(() => {
+      const product = new Product("123", "", 100);
+    }).toThrowError("Name is required");
+  });
 
-    it('should throw error when name is empty', () => {
-        expect(() => {
-            new Product('id', '', 14)
-        }).toThrowError('Name is required')
-    })
+  it("should throw error when price is less than zero", () => {
+    expect(() => {
+      const product = new Product("123", "Name", -1);
+    }).toThrowError("Price must be greater than zero");
+  });
 
-    it('should throw error when price is less than zero', () => {
-        expect(() => {
-            new Product('id', 'name', -1)
-        }).toThrowError('Price must be greater than zero')
-    })
+  it("should change name", () => {
+    const product = new Product("123", "Product 1", 100);
+    product.changeName("Product 2");
+    expect(product.name).toBe("Product 2");
+  });
 
-    it('should change name', () => {
-        const product = new Product('1', 'Product 1', 15)
-        product.changeName('Product 2')
-        expect(product.name).toBe('Product 2')
-
-        expect(() => {
-            product.changeName('')
-        }).toThrowError('Name is required')
-    })
-
-    it('should change price', () => {
-        const product = new Product('1', 'Product 1', 15)
-        product.changePrice(20)
-        expect(product.price).toBe(20)
-
-        expect(() => {
-            product.changePrice(-1)
-        }).toThrowError('Price must be greater than zero')
-    })
-
-})
+  it("should change price", () => {
+    const product = new Product("123", "Product 1", 100);
+    product.changePrice(150);
+    expect(product.price).toBe(150);
+  });
+});
